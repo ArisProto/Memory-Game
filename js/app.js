874c05b87086de44e013f3dedc3f0be3539c9887
Aris-Protopapas <aris.protopapas@gmail.com>
2018-07-01 05:37:25 +0200

    Added dove icon

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 * Create a list that holds all of your cards
 */
 
-const MyCards = ["fas fa-ambulance", "fas fa-fighter-jet", "fas fa-bolt","fas fa-child", "fas fa-chess-knight", "fas fa-archway", "fas fa-feather", "far fa-anchor"];
+const MyCards = ["fas fa-ambulance", "fas fa-fighter-jet", "fas fa-anchor","fas fa-child", "fas fa-chess-knight", "fas fa-archway", "fas fa-feather", "fas fa-dove"];
 const MyDeck = MyCards.concat(MyCards);
 const shuffleCards = shuffle(MyDeck);
 
