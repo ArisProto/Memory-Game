59c3be1a04b581aae9755bc9bae73627896e0c3e
Aris-Protopapas <aris.protopapas@gmail.com>
2018-07-02 14:55:15 +0200

    fix moves in winning screen

--- js/app.js
+++ js/app.js
@@ -216,7 +216,7 @@ function winGame() {
     // add text
     h1Html.innerText = 'Congratulations!' + ' \n ' + 'You Successfully Finished the Memory!';
     h2Html.innerText = 'Here are your stats';
-    pHtml.innerText = 'It took you ' + moves + ' moves (' + howManyStars.length + plurificationStar() + '),\n ' + minutes + ' minutes and ' + seconds + ' seconds to complete the game!';
+    pHtml.innerText = 'It took you ' + moves.innerHTML + ' moves (' + howManyStars.length + plurificationStar() + '),\n ' + minutes + ' minutes and ' + seconds + ' seconds to complete the game!';
 
     function plurificationStar(){
      if(howManyStars.length === 1){
